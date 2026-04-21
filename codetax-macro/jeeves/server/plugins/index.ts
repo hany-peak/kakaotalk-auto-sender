27c504170be712de36872077a67d55e2bdd0e631
hany <hsi05277@gmail.com>
2026-04-22 01:13:34 +0900

    feat(new-client): register plugin in server plugin list

--- codetax-macro/jeeves/server/plugins/index.ts
+++ codetax-macro/jeeves/server/plugins/index.ts
@@ -3,10 +3,12 @@ import { vatNoticePlugin } from './vat-notice';
 import { kakaoSendPlugin } from './kakao-send';
 import { messagesPlugin } from './messages';
 import { thebillSyncPlugin } from './thebill-sync';
+import { newClientPlugin } from './new-client';
 
 export const plugins: MacroPlugin[] = [
   vatNoticePlugin,
   kakaoSendPlugin,
   messagesPlugin,
   thebillSyncPlugin,
+  newClientPlugin,
 ];
